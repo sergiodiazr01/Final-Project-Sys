1a53c9d687137ba4e41860a256250927dd9f8be6
AlexMartinez7 <alex.martinez12@estudiant.upf.edu>
2025-05-15 13:01:35 +0200

    powerup box spawner añadido

---
+++ Assets/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour
+{
+    public float minX = -44f, maxX = 44f;
+    public float minZ = -24f, maxZ = 24f;
+    public float spawnY = 0.5f;
+
+    public GameObject powerUpBoxPrefab;
+    public float spawnInterval = 10f; // tiempo entre spawns
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnPowerUpBox), 2f, spawnInterval);
+    }
+
+    private void SpawnPowerUpBox()
+    {
+        if (powerUpBoxPrefab == null) return;
+
+        float randomX = Random.Range(minX, maxX);
+        float randomZ = Random.Range(minZ, maxZ);
+        Vector3 spawnPos = new Vector3(randomX, spawnY, randomZ);
+
+        Instantiate(powerUpBoxPrefab, spawnPos, Quaternion.identity);
+        Debug.Log("Se ha spawneado una PowerUp Box en " + spawnPos);
+    }
+}
